9d33490e3f92944b2152acb25b5df6214376d808
Mike Frost <me@mfrost.co.uk>
2024-09-28 04:13:19 +0100

    fix: Checkin script fixes

--- checkin.sh
+++ checkin.sh
@@ -5,7 +5,7 @@ MYIP=$(ip route show default | sed 's/.*src //' | sed 's/ .*//')
 echo "My IP: $MYIP"
 MYMAC=$(ip addr show | grep $MYIP -B1 | head -n1 | sed 's/.*ether //' | sed 's/ .*//')
 echo "My MAC: $MYMAC"
-MYPIKIOSKVERSION=$(git -C /opt/pikiosk/ describe --tags)
+MYPIKIOSKVERSION=$(/usr/bin/git -C /opt/pikiosk/ describe --tags)
 echo "My Pikiosk Version: $MYPIKIOSKVERSION"
 MYOS=$(cat /etc/os-release | grep PRETTY_NAME | sed 's/.*AME..\(.*\).$/\1/')
 echo "My OS: $MYOS"
